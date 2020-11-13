4b3606a0269b84029ec6adb65af32a13ccd35c3b
SergeyAverin <ct.averin@yandex.ru>
2020-11-13 17:16:54 +0600

    add abstract user

--- socialNetwork/main/models.py
+++ socialNetwork/main/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class AbstUser(AbstractUser):
+    GENDERS = [
+        ('male', 'male'),
+        ('female', 'female')
+    ]
+
+    about_yourself = models.TextField(
+        'о себе',
+        default=' ',
+        blank=False
+    )
+
+    gender = models.CharField(
+        'Пол',
+        choices=GENDERS,
+        max_length=7,
+        default='male',
+        blank=False
+    )
+    
+    class Meta(AbstractUser.Meta):
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
