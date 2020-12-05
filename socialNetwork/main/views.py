a358da820dc41eca91eb5fce9099b5817b3292d3
SergeyAverin <ct.averin@yandex.ru>
2020-12-05 22:27:36 +0600

    add user api

--- socialNetwork/main/views.py
+++ socialNetwork/main/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.http import Http404
+from rest_framework import status
 
-# Create your views here.
+from .models import AbstUser
+from .serializers import AbstUserSerializer
+
+
+class UserByUserNameView(APIView):
+    def get(self, request, username, format=None):
+        user = AbstUser.objects.get(username= username)
+        serializer = AbstUserSerializer(user)
+        return Response(serializer.data)
+
+    def put(self, request, username, format=None):
+        user = AbstUser.objects.get(username= username)
+        serializer = AbstUserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, username, format=None):
+        user = AbstUser.objects.get(username= username)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- socialNetwork/socialNetwork/urls.py
+++ socialNetwork/socialNetwork/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('frontend.urls'))
+    path('', include('frontend.urls')),
+    path('api/v1.0/', include('main.urls'))
 ]
