b937ea107b7636d44b801b98e7322f30e7fc7a00
SergeyAverin <ct.averin@yandex.ru>
2020-12-06 20:36:32 +0600

    Update serializers.py

--- socialNetwork/main/serializers.py
+++ socialNetwork/main/serializers.py
@@ -3,14 +3,6 @@ from rest_framework import serializers
 
 
 class AbstUserSerializer(serializers.ModelSerializer):
-    #username = serializers.CharField()
-    #lastName = serializers.CharField()
-    #firstName = serializers.CharField()
-    #email = serializers.EmailField()
-    #about_yourself = serializers.CharField()
-    #gender = serializers.c
-
-
     class Meta:
         model = AbstUser
         fields = ['username', 'last_name', 'first_name', 'email', 'about_yourself']
